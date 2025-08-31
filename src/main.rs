11dd5ab179e24a8f72808364eb27fcc1a743315a
Jakub Latusek <jk.latusek@gmail.com>
2025-08-31 16:38:11 +0200

    make main cleaner

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,22 @@ pub mod dir_scanner;
 pub mod parser;
 
 fn main() {
-    let paths = list_files(
-        env::args()
-            .skip(1)
-            .next()
-            .expect("Not enough arguments")
-            .as_str(),
-    );
-    let mut js_functions: Vec<JSFunction> = Vec::new();
-    for path in paths {
-        js_functions.append(&mut parser::JSParser::new().parse_file(&path));
-    }
+    let directory = env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("Usage: js_parser <directory>");
+        std::process::exit(1);
+    });
 
-    let json = serde_json::to_string_pretty(&js_functions).expect("Error to json");
+    let paths = list_files(directory, "js");
+
+    // TODO: Add parallel processing of JS files
+    let js_functions: Vec<JSFunction> = paths
+        .into_iter()
+        .flat_map(|path| parser::JSParser::new().parse_file(&path))
+        .collect();
+
+    let json = serde_json::to_string_pretty(&js_functions).unwrap_or_else(|e| {
+        eprintln!("Error serializing to JSON: {}", e);
+        std::process::exit(1);
+    });
     println!("{}", json);
 }
