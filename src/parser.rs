48369ce7f5fd89eacd8c909826ceff1e3615a6d2
Jakub Latusek <jk.latusek@gmail.com>
2025-08-31 20:03:49 +0200

    Add proper error handling, use id instead of string names

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,20 @@
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::PathBuf;
+use thiserror::Error;
 use tree_sitter::Parser;
 
-pub struct JSParser {
-    parser: Parser,
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("Failed to read file: {0}")]
+    FileRead(#[from] std::io::Error),
+    #[error("Failed to parse JavaScript: {0}")]
+    TreeSitter(String),
+    #[error("Failed to set language")]
+    LanguageSetup,
 }
+
+pub type Result<T> = std::result::Result<T, ParseError>;
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct SerPoint {
@@ -22,7 +31,6 @@ impl From<tree_sitter::Point> for SerPoint {
     }
 }
 
-// TODO: difference between String and string!!!
 #[derive(Serialize, Deserialize, Debug)]
 pub struct JSFunction {
     pub identifier: String,
@@ -32,45 +40,71 @@ pub struct JSFunction {
     pub end: SerPoint,
 }
 
+pub struct JSParser {
+    parser: Parser,
+    function_kind_id: u16,
+    name_field_id: u16,
+}
+
 impl JSParser {
-    pub fn new() -> Self {
+    pub fn new() -> Result<Self> {
         let mut parser = Parser::new();
+        let language = &tree_sitter_javascript::LANGUAGE.into();
         parser
-            .set_language(&tree_sitter_javascript::LANGUAGE.into())
-            .expect("Error loading JavaScript grammar");
-        Self { parser }
+            .set_language(&language)
+            .map_err(|_| ParseError::LanguageSetup)?;
+
+        let function_kind_id = language.id_for_node_kind("function_declaration", true);
+        let name_field_id = language.field_id_for_name("name").ok_or_else(|| {
+            ParseError::TreeSitter("Field 'name' not found in language".to_string())
+        })?;
+
+        Ok(Self {
+            parser,
+            function_kind_id,
+            name_field_id: name_field_id.into(),
+        })
     }
 
-    pub fn parse_file(&mut self, path: &PathBuf) -> Vec<JSFunction> {
+    pub fn parse_file(&mut self, path: &PathBuf) -> Result<Vec<JSFunction>> {
         let mut js_functions: Vec<JSFunction> = Vec::new();
-        let source = fs::read_to_string(&path).expect("Cannot read file :((");
-        let tree = self.parser.parse(&source, None).unwrap();
-        let root_node = tree.root_node();
-        let mut cursor = root_node.walk();
-        for child in root_node.named_children(&mut cursor) {
-            // TODO: replace string compassion with kind_id
-            if child.kind() == "function_declaration" {
-                let mut gname: String = String::new();
-                if let Some(name) = self.extract_function_name(&child, &source) {
-                    gname = name;
-                }
+        let source = fs::read_to_string(&path)?;
 
-                js_functions.push(JSFunction {
-                    identifier: format!("{}:{}", path.display(), child.start_position().row + 1,),
-                    name: String::from(gname),
-                    // TODO: do we really need clone here?
-                    path: path.display().to_string(),
-                    start: child.start_position().into(),
-                    end: child.end_position().into(),
-                })
-            };
-        }
-        return js_functions;
+        let tree = self
+            .parser
+            .parse(&source, None)
+            .ok_or_else(|| ParseError::TreeSitter("Error during parsing a tree".to_string()))?;
+        let root_node = tree.root_node();
+
+        let mut cursor = root_node.walk();
+        js_functions.extend(
+            root_node
+                .named_children(&mut cursor)
+                .filter(|child| child.kind_id() == self.function_kind_id)
+                .filter_map(|child| self.create_js_function(&child, &source, path)),
+        );
+        Ok(js_functions)
+    }
+
+    fn create_js_function(
+        &self,
+        node: &tree_sitter::Node,
+        source: &str,
+        path: &PathBuf,
+    ) -> Option<JSFunction> {
+        let name = self.extract_function_name(node, source)?;
+
+        Some(JSFunction {
+            identifier: format!("{}:{}", path.display(), node.start_position().row + 1),
+            name,
+            path: path.display().to_string(),
+            start: node.start_position().into(),
+            end: node.end_position().into(),
+        })
     }
 
     fn extract_function_name(&self, node: &tree_sitter::Node, source: &str) -> Option<String> {
-        // Get the child by its field name ("name" works in JS grammar)
-        if let Some(ident) = node.child_by_field_name("name") {
+        if let Some(ident) = node.child_by_field_id(self.name_field_id) {
             return ident
                 .utf8_text(source.as_bytes())
                 .ok()
