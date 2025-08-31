acc896eedef4a897ec8034f67efa62aedc1a89fd
Jakub Latusek <jk.latusek@gmail.com>
2025-08-31 16:12:11 +0200

    dir_scanner: add better error handling with proper messages

--- src/dir_scanner.rs
+++ src/dir_scanner.rs
@@ -1,14 +1,24 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
 // Why we are using here PathBuf instead of Path???
-pub fn list_files(directory: &str) -> Vec<PathBuf> {
+pub fn list_files<P: AsRef<Path>>(directory: P, extension: &str) -> Vec<PathBuf> {
     WalkDir::new(directory)
         .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| {
-            e.file_type().is_file()
-                && e.path().extension().and_then(|ext| ext.to_str()) == Some("js")
+        .filter_map(|e| match e {
+            Ok(entry) => Some(entry),
+            Err(err) => {
+                eprintln!("Error accessing directory entry: {}", err);
+                None
+            }
+        })
+        .filter(|entry| {
+            entry.file_type().is_file()
+                && entry
+                    .path()
+                    .extension()
+                    .and_then(|ext| ext.to_str())
+                    .map_or(false, |ext| ext.eq_ignore_ascii_case(extension))
         })
         .map(|e| e.into_path())
         .collect()
